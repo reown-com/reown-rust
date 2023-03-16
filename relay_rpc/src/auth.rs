aad0c661e737705647f8498d0af473c70d9ef36c
Rakowskiii <raku@walletconnect.com>
2023-03-16 12:57:19 +0100

    feat: added jwt decoding

--- relay_rpc/src/auth.rs
+++ relay_rpc/src/auth.rs
@@ -1,5 +1,5 @@
 use {
-    crate::domain::{AuthSubject, DecodedClientId},
+    crate::domain::{AuthSubject, ClientId, ClientIdDecodingError, DecodedClientId},
     chrono::{DateTime, Utc},
     ed25519_dalek::{ed25519::signature::Signature, Keypair, Signer},
     serde::{Deserialize, Serialize},
@@ -114,12 +114,28 @@ pub struct JwtClaims<'a> {
 }
 
 impl<'a> JwtClaims<'a> {
-    pub fn is_valid(&self, aud: &HashSet<String>, time_leeway: impl Into<Option<i64>>) -> bool {
+    pub fn is_valid(
+        &self,
+        aud: &HashSet<String>,
+        time_leeway: impl Into<Option<i64>>,
+    ) -> Result<(), JwtVerificationError> {
         let time_leeway = time_leeway
             .into()
             .unwrap_or(JWT_VALIDATION_TIME_LEEWAY_SECS);
         let now = Utc::now().timestamp();
-        (now + time_leeway) >= self.iat && (now - time_leeway) <= self.exp && aud.contains(self.aud)
+
+        if !(now - time_leeway <= self.exp) {
+            return Err(JwtVerificationError::Expired);
+        }
+
+        if !(now + time_leeway >= self.iat) {
+            return Err(JwtVerificationError::NotYetValid);
+        }
+
+        if !aud.contains(self.aud) {
+            return Err(JwtVerificationError::InvalidAudience);
+        }
+        Ok(())
     }
 }
 
@@ -169,4 +185,126 @@ pub fn encode_auth_token(
     };
 
     Ok(SerializedAuthToken(format!("{message}.{signature}")))
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum JwtVerificationError {
+    #[error("Invalid format")]
+    Format,
+
+    #[error("Invalid encoding")]
+    Encoding,
+
+    #[error("Invalid JWT signing algorithm")]
+    Header,
+
+    #[error("JWT Token is expired")]
+    Expired,
+
+    #[error("JWT Token is not yet valid")]
+    NotYetValid,
+
+    #[error("Invalid audience")]
+    InvalidAudience,
+
+    #[error("Invalid signature")]
+    Signature,
+
+    #[error("Invalid JSON")]
+    Serialization,
+
+    #[error("Invalid issuer DID prefix")]
+    IssuerPrefix,
+
+    #[error("Invalid issuer DID method")]
+    IssuerMethod,
+
+    #[error("Invalid issuer format")]
+    IssuerFormat,
+
+    #[error(transparent)]
+    PubKey(#[from] ClientIdDecodingError),
+}
+
+#[derive(Debug)]
+pub struct Jwt(pub String);
+
+impl Jwt {
+    pub fn decode(&self, aud: &HashSet<String>) -> Result<ClientId, JwtVerificationError> {
+        let mut parts = self.0.splitn(3, JWT_DELIMITER);
+
+        let (Some(header), Some(claims)) = (parts.next(), parts.next()) else {
+            return Err(JwtVerificationError::Format);
+        };
+
+        let decoder = &data_encoding::BASE64URL_NOPAD;
+
+        let header_len = decoder
+            .decode_len(header.len())
+            .map_err(|_| JwtVerificationError::Encoding)?;
+        let claims_len = decoder
+            .decode_len(claims.len())
+            .map_err(|_| JwtVerificationError::Encoding)?;
+
+        let mut output = vec![0u8; header_len.max(claims_len)];
+
+        // Decode header.
+        data_encoding::BASE64URL_NOPAD
+            .decode_mut(header.as_bytes(), &mut output[..header_len])
+            .map_err(|_| JwtVerificationError::Encoding)?;
+
+        {
+            let header = serde_json::from_slice::<JwtHeader>(&output[..header_len])
+                .map_err(|_| JwtVerificationError::Serialization)?;
+
+            if !header.is_valid() {
+                return Err(JwtVerificationError::Header);
+            }
+        }
+
+        // Decode claims.
+        data_encoding::BASE64URL_NOPAD
+            .decode_mut(claims.as_bytes(), &mut output[..claims_len])
+            .map_err(|_| JwtVerificationError::Encoding)?;
+
+        let claims = serde_json::from_slice::<JwtClaims>(&output[..claims_len])
+            .map_err(|_| JwtVerificationError::Serialization)?;
+
+        // Basic token validation: `iat`, `exp` and `aud`.
+        claims.is_valid(aud, None)?;
+
+        let did_key = claims
+            .iss
+            .strip_prefix(DID_PREFIX)
+            .ok_or(JwtVerificationError::IssuerPrefix)?
+            .strip_prefix(DID_DELIMITER)
+            .ok_or(JwtVerificationError::IssuerFormat)?
+            .strip_prefix(DID_METHOD)
+            .ok_or(JwtVerificationError::IssuerMethod)?
+            .strip_prefix(DID_DELIMITER)
+            .ok_or(JwtVerificationError::IssuerFormat)?;
+
+        let pub_key = did_key.parse::<DecodedClientId>()?;
+
+        let mut parts = self.0.rsplitn(2, JWT_DELIMITER);
+
+        let (Some(signature), Some(message)) = (parts.next(), parts.next()) else {
+            return Err(JwtVerificationError::Format);
+        };
+
+        let key = jsonwebtoken::DecodingKey::from_ed_der(pub_key.as_ref());
+
+        // Finally, verify signature.
+        let sig_result = jsonwebtoken::crypto::verify(
+            signature,
+            message.as_bytes(),
+            &key,
+            jsonwebtoken::Algorithm::EdDSA,
+        );
+
+        match sig_result {
+            Ok(true) => Ok(pub_key.into()),
+            _ => Err(JwtVerificationError::Signature),
+        }
+    }
 }
