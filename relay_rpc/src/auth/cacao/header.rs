82a1b265a168ba75a1a630f6158aad8e1cd71f6c
Chris Smith <chris@walletconnect.com>
2024-02-22 14:38:07 -0500

    chore: improve header error

--- relay_rpc/src/auth/cacao/header.rs
+++ relay_rpc/src/auth/cacao/header.rs
@@ -1,20 +1,21 @@
 use {
     super::CacaoError,
     serde::{Deserialize, Serialize},
+    std::sync::Arc,
 };
 
 pub const EIP4361: &str = "eip4361";
 
 #[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize, Hash)]
 pub struct Header {
-    pub t: String,
+    pub t: Arc<str>,
 }
 
 impl Header {
     pub fn validate(&self) -> Result<(), CacaoError> {
-        match self.t.as_str() {
+        match self.t.as_ref() {
             EIP4361 => Ok(()),
-            _ => Err(CacaoError::Header),
+            _ => Err(CacaoError::HeaderTypeUnsupported(self.t.clone())),
         }
     }
 }
